b44f1f0aed9b010254aec7436c4e7dfaaeb72fac
Sharmila Prasath <sharmilasri03@gmail.com>
2021-02-11 02:38:36 -0800

    Validations added

--- sample_app/app/src/main/java/com/sharmila/android/sampleapp/DisplayListActivity.java
+++ sample_app/app/src/main/java/com/sharmila/android/sampleapp/DisplayListActivity.java
@@ -10,6 +10,7 @@ import android.os.Bundle;
 
 import com.sharmila.android.sampleapp.adapaters.HotelListAdapter;
 import com.sharmila.android.sampleapp.model.HotelList;
+import com.sharmila.android.sampleapp.utils.UtilityFunctions;
 import com.sharmila.android.sampleapp.viewmodel.HotelViewModel;
 
 public class DisplayListActivity extends AppCompatActivity {
@@ -28,16 +29,20 @@ public class DisplayListActivity extends AppCompatActivity {
     }
 
     private void setUpViews() {
-        //set up recycler
-        recyclerView = findViewById(R.id.recycle_view_hotel_list);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        if(UtilityFunctions.isNetworkConnected(this)) {//check for internet
+            //set up recycler
+            recyclerView = findViewById(R.id.recycle_view_hotel_list);
+            recyclerView.setHasFixedSize(true);
+            recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        //setup view model and network call
-        HotelViewModel model = new ViewModelProvider(this).get(HotelViewModel.class);
-        model.getHotels().observe(this, hotels -> {
-            adapter = new HotelListAdapter(DisplayListActivity.this, hotels.getHotels());
-            recyclerView.setAdapter(adapter);
-        });
+            //setup view model and network call
+            HotelViewModel model = new ViewModelProvider(this).get(HotelViewModel.class);
+            model.getHotels().observe(this, hotels -> {
+                if(hotels!=null) {//check if list is not null
+                    adapter = new HotelListAdapter(DisplayListActivity.this, hotels.getHotels());
+                    recyclerView.setAdapter(adapter);
+                }
+            });
+        }
     }
 }
